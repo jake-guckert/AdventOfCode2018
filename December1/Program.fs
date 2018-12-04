5508f794f73d840702e7b903c834a52b472f9ccb
Jake Guckert <jguckert@inntopia.com>
2018-12-03 23:25:26 -0500

    commit for first half of day 1

--- December1/Program.fs
+++ December1/Program.fs
@@ -1,8 +1,21 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+
+ let op (a:char, b:int) : int = 
+    match a with
+     | '+' -> b
+     | '-' -> b*(-1)
+     | _ -> b
+
+let strToNum (s:string) : int = 
+    let r = op(s.[0], s.[1..] |> int)
+    r
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    File.ReadLines "input.txt"
+    |> Seq.fold (fun acc elem -> acc + strToNum(elem)) 0
+    |> printfn "%d"
+    0
